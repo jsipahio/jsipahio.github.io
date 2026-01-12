b8ed249587ba3ef269e425c2ac30d0559acd46e5
jsipahio <116314200+jsipahio@users.noreply.github.com>
2026-01-12 13:31:30 +0000

    Start new mkdocs site

--- site/src/nav.jsx
+++
@@ -1,9 +0,0 @@
-
-export function Navigation() {
-    return (
-        <nav className="flex justify-center space-x-4">
-            <a href="https://github.com" className="rounded-lg px-3 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900">GitHub</a>
-            <a href="https://www.kent.edu" className="rounded-lg px-3 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900">Kent</a>
-        </nav>
-    )
-}
