84a26c3ae2dddb557b25d4421f38239d5129e8e1
jsipahio <jsipahio@kent.edu>
2026-01-28 11:42:19 -0500

    Deployed cc19d45 with MkDocs version: 1.6.1

---
+++ research/wrapper-survey/post.js
@@ -0,0 +1,17 @@
+
+class Demographic {
+
+}
+
+class LikertScale {
+    
+}
+
+function registerSubmit(id) {
+    document.addEventListener(document.getElementById(id), validateForm);
+}
+
+function validateForm() {
+    const form = document.getElementById("entry-form");
+    
+}
